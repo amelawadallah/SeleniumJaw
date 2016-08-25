27d55168af7b0d59ca1338e5071a152c0112b026
Amel Awadallah <Amel Awadallah@10.0.0.3>
2016-08-25 22:42:33 +0200

    second commit test

--- src/main/java/com/utils/DriverFactory.java
+++ src/main/java/com/utils/DriverFactory.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
-
+//testing github
 	 public enum BrowserType{
 		 FIREFOX,
 		 CHROME,
